52807658a437ccb85375e30a72935aaa443cdd68
Luis Armuto <luis.backend.fractalup@gmail.com>
2024-08-12 20:51:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Card } from './components/index.js'
+import './app.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='title'>Carga de estudiantes</h1>
+      <form>
+        
+      </form>
+      <Card/>      
+    </>
+  )
+}
+
+export default App
